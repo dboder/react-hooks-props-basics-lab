ce716193f0928695d5a1adc2a8ca99d9e98566b5
Dorsey B <dorseybec@gmail.com>
2023-04-29 15:38:24 -0700

    complete

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,22 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About(props) {
+  const usrBio = props.bio;
+  let bio_paragraph;
+  if (usrBio && usrBio.length > 0)
+  {
+    bio_paragraph = <p>{usrBio}</p>;
+  }
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {bio_paragraph}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github = {props.github} linkedin={props.linkedin}/>
     </div>
   );
 }
+
 
 export default About;
